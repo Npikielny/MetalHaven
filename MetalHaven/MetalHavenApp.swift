7a3c825ed64ad824d08fb3bc5bf8febc11a76799
Noah Pikielny <np299@cornell.edu>
2023-07-02 17:35:48 -0400

    Initial Commit

---
+++ MetalHaven/MetalHavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalHavenApp.swift
+//  MetalHaven
+//
+//  Created by Noah Pikielny on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MetalHavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
